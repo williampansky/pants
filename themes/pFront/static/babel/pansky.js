6584056cd1196bf573ca36446e20bbaef0238319
williampansky <williampansky@gmail.com>
2017-06-20 13:56:54 -0500

    v2.0.3
    Added pansy-boilerplate to projects list. Adjust homepage mobile button
    visibility. Added some velocity.js animations. Changed sidebar.

--- themes/pFront/static/babel/pansky.js
+++ themes/pFront/static/babel/pansky.js
@@ -1,3 +1,82 @@
+// $(document).ready( sidebarBlink );
+//
+// function sidebarBlink() {
+//   $( '.tm-sidebar-left' ).css( 'opacity', 0.75 );
+//   $( '.tm-sidebar-left' ).css( 'opacity', 0.15 );
+// }
+
+
+// remove mobile toggle button on homepage
+$( () => {
+  if ( $( 'section' ).hasClass( 'pansky-home' ) ) {
+    $( '.uk-navbar-toggle' ).remove();
+  } else {
+    null
+  }
+});
+
+// load .pansky-project-vendor-logo imgs velocity style!
+$('.pansky-project-vendor-logo').velocity( 'transition.fadeIn', {
+  stagger: 300,
+  drag: true,
+});
+
+
+
+
+// ==============================================================
+//
+// # ALLOW KEYBOARD TO ACTIVE DESKTOP OFFCANVAS SIDEBAR
+// https://stackoverflow.com/a/30390887
+//
+// ==============================================================
+var deepFocus = function(element, cls) {
+  cls = cls || 'deep-focus';
+
+  element
+    .on('focusin', function() {
+      element.addClass(cls);
+    })
+    .on('focusout', function() {
+      var value = !!element.find(':focus').length;
+      element.toggleClass(cls, value);
+    });
+};
+
+$(document).ready(function() {
+  deepFocus($('#offcanvas-desktop'));
+});
+
+
+
+
+// ==============================================================
+//
+// # pansky-project-readmore VELOCITY ANIMATIONS
+//
+// ==============================================================
+$(function() {
+  var readMore = $('.pansky-project-readmore');
+  var moreWrapper = $('.uk-accordion-content');
+  var moreContent = $('.uk-accordion-content > div, .uk-accordion-content > p');
+
+  readMore.on('click', function() {
+    if ( $('readMore').parent().hasClass( 'uk-open' ) == false ) {
+      moreWrapper.velocity( 'transition.fadeIn' )
+      moreContent.velocity( 'transition.slideDownIn', {
+        stagger: 100,
+        drag: true,
+        display: "flex"
+      });
+    } else {
+      moreWrapper.css( 'opacity', 0 );
+      moreContent.css( 'opacity', 0 );
+    }
+  });
+});
+
+
+
 // // #mobileNav controls
 // // $('.uk-navbar-toggle').on('click', function() {
 // //   if ($('#offcanvas').hasClass('uk-open') == true) {
