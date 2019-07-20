6502c181d2fa4a34ea3e409eae345b99004cf5e1
William Pansky <williampansky@gmail.com>
2019-07-20 11:40:18 -0500

    Added createPage actions loop to node config to auto-generate pages based on query results

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,32 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path');
+exports.createPages = ({ actions, graphql }) => {
+    const { createPage } = actions;
+    const template = path.resolve(`src/templates/posts.jsx`);
+    return graphql(`
+        {
+            allContentfulCaseStudy {
+                edges {
+                    node {
+                        slug
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        if (result.errors) return Promise.reject(result.errors);
+        result.data.allContentfulCaseStudy.edges.forEach(({ node }) => {
+            if (node) {
+                createPage({
+                    path: `/posts/${node.slug}`,
+                    component: template,
+                    context: {
+                        slug: node.slug
+                    }
+                });
+            }
+        });
+    });
+};
