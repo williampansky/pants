d171d5228cc6cbe4497aaf408ce077760bc98c76
William Pansky <williampansky@gmail.com>
2019-07-20 11:13:40 -0500

    Updated all imports to the renamed component files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,148 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+import Layout from '@/components/Layout';
+import SEO from '@/components/SEO';
 
-import Layout from '@/components/layout';
-import Image from '@/components/image';
-import SEO from '@/components/seo';
-
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
     <Layout>
         <SEO title="Home" />
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-            <Image />
-        </div>
-        <Link to="/page-2/">Go to page 2</Link>
+        <div>{data.allContentfulBlogPosts.nodes[0].featuredPost.title}</div>
+        {data.allContentfulBlogPosts.nodes[0].allPosts.map(node => (
+            <div key={node.id}>{node.title}</div>
+        ))}
     </Layout>
 );
+
+export const pageQuery = graphql`
+    query HomepageContent {
+        allContentfulBlogPosts {
+            nodes {
+                featuredPost {
+                    id
+                    title
+                    client {
+                        name
+                        website
+                        description {
+                            description
+                        }
+                        logo {
+                            localFile {
+                                childImageSharp {
+                                    fluid {
+                                        ...GatsbyImageSharpFluid
+                                    }
+                                    original {
+                                        height
+                                        width
+                                    }
+                                }
+                            }
+                        }
+                        avatar {
+                            localFile {
+                                childImageSharp {
+                                    fluid(jpegProgressive: true, quality: 65) {
+                                        ...GatsbyImageSharpFluid
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    project {
+                        title
+                        website
+                        repository
+                        description {
+                            description
+                        }
+                    }
+                    leadParagraph {
+                        leadParagraph
+                    }
+                    introduction {
+                        introduction
+                    }
+                    objective {
+                        objective
+                    }
+                    development {
+                        development
+                    }
+                    conclusion {
+                        conclusion
+                    }
+                    sources {
+                        sources
+                    }
+                }
+                allPosts {
+                    id
+                    title
+                    client {
+                        name
+                        website
+                        description {
+                            description
+                        }
+                        logo {
+                            localFile {
+                                childImageSharp {
+                                    fluid {
+                                        ...GatsbyImageSharpFluid
+                                    }
+                                    original {
+                                        height
+                                        width
+                                    }
+                                }
+                            }
+                        }
+                        avatar {
+                            localFile {
+                                childImageSharp {
+                                    fluid(jpegProgressive: true, quality: 65) {
+                                        ...GatsbyImageSharpFluid
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    project {
+                        title
+                        website
+                        repository
+                        description {
+                            description
+                        }
+                    }
+                    leadParagraph {
+                        leadParagraph
+                    }
+                    introduction {
+                        introduction
+                    }
+                    objective {
+                        objective
+                    }
+                    development {
+                        development
+                    }
+                    conclusion {
+                        conclusion
+                    }
+                    sources {
+                        sources
+                    }
+                }
+            }
+        }
+    }
+`;
+
+IndexPage.propTypes = {
+    data: PropTypes.object.isRequired
+};
 
 export default IndexPage;
