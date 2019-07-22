f3bbd5a4dc263ca4802ff652dc9623bbc1964f03
William Pansky <williampansky@gmail.com>
2019-07-22 09:32:17 -0500

    Added markdown transformer and prismjs

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,8 @@
 const path = require(`path`);
 require('regenerator-runtime/runtime');
 require('dotenv').config({ path: `.env` });
+// require('prismjs/themes/prism-dark.css');
+// require('prismjs/plugins/line-numbers/prism-line-numbers.css');
 
 module.exports = {
     siteMetadata: {
@@ -162,6 +164,69 @@ module.exports = {
         `gatsby-plugin-sharp`,
 
         /**
+         * Adds syntax highlighting to code blocks
+         * in markdown files using [PrismJS].
+         *
+         * @name gatsby-remark-prismjs
+         * @see https://www.gatsbyjs.org/packages/gatsby-remark-prismjs
+         * [PrismJS]: https://prismjs.com/
+         */
+        {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+                // Class prefix for <pre> tags containing syntax highlighting;
+                // defaults to 'language-' (eg <pre class="language-js">).
+                // If your site loads Prism into the browser at runtime,
+                // (eg for use with libraries like react-live),
+                // you may use this to prevent Prism from re-processing syntax.
+                // This is an uncommon use-case though;
+                // If you're unsure, it's best to use the default value.
+                classPrefix: 'language-',
+                // This is used to allow setting a language for inline code
+                // (i.e. single backticks) by creating a separator.
+                // This separator is a string and will do no white-space
+                // stripping.
+                // A suggested value for English speakers is the non-ascii
+                // character '›'.
+                inlineCodeMarker: null,
+                // This lets you set up language aliases.  For example,
+                // setting this to '{ sh: "bash" }' will let you use
+                // the language "sh" which will highlight using the
+                // bash highlighter.
+                aliases: {},
+                // This toggles the display of line numbers globally alongside the code.
+                // To use it, add the following line in src/layouts/index.js
+                // right after importing the prism color scheme:
+                //  `require("prismjs/plugins/line-numbers/prism-line-numbers.css");`
+                // Defaults to false.
+                // If you wish to only show line numbers on certain code blocks,
+                // leave false and use the {numberLines: true} syntax below
+                showLineNumbers: false,
+                // If setting this to true, the parser won't handle and highlight inline
+                // code used in markdown i.e. single backtick code like `this`.
+                noInlineHighlight: false,
+                // This adds a new language definition to Prism or extend an already
+                // existing language definition. More details on this option can be
+                // found under the header "Add new language definition or extend an
+                // existing language" below.
+                languageExtensions: [
+                    {
+                        language: 'superscript',
+                        extend: 'javascript',
+                        definition: {
+                            superscript_types: /(SuperType)/
+                        },
+                        insertBefore: {
+                            function: {
+                                superscript_keywords: /(superif|superelse)/
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+
+        /**
          * A Gatsby source plugin for sourcing data into your
          * Gatsby application from your local filesystem.
          *
@@ -173,6 +238,29 @@ module.exports = {
             options: {
                 name: `images`,
                 path: path.join(__dirname, `src`, `images`)
+            }
+        },
+
+        /**
+         * Parses Markdown files using [Remark].
+         *
+         * @name gatsby-transformer-remark
+         * @see https://www.gatsbyjs.org/packages/gatsby-transformer-remark/
+         * [Remark]: https://remark.js.org/
+         */
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                // CommonMark mode (default: true)
+                commonmark: true,
+                // Footnotes mode (default: true)
+                footnotes: true,
+                // Pedantic mode (default: true)
+                pedantic: true,
+                // GitHub Flavored Markdown mode (default: true)
+                gfm: true,
+                // Plugins configs
+                plugins: []
             }
         },
 
