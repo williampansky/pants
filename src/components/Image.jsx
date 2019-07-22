9bf1b0b348bf2ac621eb06b1ad534bf7981cebda
William Pansky <williampansky@gmail.com>
2019-07-22 09:32:32 -0500

    Modified Image component to accept src and alt props

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+// import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
 /*
@@ -13,22 +13,8 @@ import Img from 'gatsby-image';
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
-    const data = useStaticQuery(graphql`
-        query {
-            placeholderImage: file(
-                relativePath: { eq: "gatsby-astronaut.png" }
-            ) {
-                childImageSharp {
-                    fluid(maxWidth: 300) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-        }
-    `);
-
-    return <Img fluid={data.placeholderImage.childImageSharp.fluid} />;
+const Image = ({ src, alt }) => {
+    return <Img fluid={src} alt={alt} />;
 };
 
 export default Image;
